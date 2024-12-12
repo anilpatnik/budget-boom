4a348c804d06fee3808778d95271e399393a06a2
Anil Patnaik <anil.patnik@gmail.com>
2024-12-13 03:18:31 +1100

    dev in progress

--- client/src/layouts/navigation.tsx
+++ client/src/layouts/navigation.tsx
@@ -51,7 +51,7 @@ function TabLayout() {
       <IonPage id="main-content">
         <Header />
         <TabMenu>
-          <IonRouterOutlet>
+          <IonRouterOutlet aria-hidde="false">
             <IonContent className="custom-content">
               <Outlet />
             </IonContent>
