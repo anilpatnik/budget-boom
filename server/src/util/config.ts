167051f2501f9c670a93d0bfda78fbd08fda0ed8
Anil Patnaik <anil.patnik@gmail.com>
2024-12-16 01:06:17 +1100

    v1.4

--- server/src/util/config.ts
+++ server/src/util/config.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+
 export const ENVIRONMENT = process.env.ENVIRONMENT;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET;
 export const CAPTCHA_SECRET = process.env.CAPTCHA_SECRET;
