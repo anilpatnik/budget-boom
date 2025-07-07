2c7fbaa736645c16d6f19d42895833133875e945
Anil Patnaik <anil.patnik@gmail.com>
2025-07-08 01:36:14 +1000

    refactored

--- server/src/util/config.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-export const ENVIRONMENT = process.env.ENVIRONMENT;
-export const TOKEN_SECRET = process.env.TOKEN_SECRET;
-export const CAPTCHA_SECRET = process.env.CAPTCHA_SECRET;
-export const DATABASE_URL = process.env.DATABASE_URL;
--- server/src/util/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./extensions";
-export * from "./config";
-export * from "./enums";
-export { errorHandler } from "./error.handler";
-export * as fb from "./firebase";
-export * as helper from "./helper";
-export * as authelper from "./auth.helper";
-export * as dbhelper from "./db.helper";
