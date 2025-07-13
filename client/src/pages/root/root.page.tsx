ca8e4680d0f13fe47b0c7804f4e81ca01f369525
Anil Patnaik <anil.patnik@gmail.com>
2025-07-14 01:56:35 +1000

    lucide icon refactor

--- client/src/pages/root/root.page.tsx
+++ client/src/pages/root/root.page.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { IonSpinner } from "@ionic/react";
 import { constants } from "@/utils";
 import { NavType, RoleType } from "@/utils/enums";
@@ -7,13 +6,12 @@ import { useStore } from "@/contexts";
 
 export function RootPage() {
   const { user } = useStore();
-  const navigate = useNavigate();
   useEffect(() => {
     if (user.auth && user.role === RoleType.Admin) {
-      setTimeout(() => navigate(NavType.Profile, { replace: true }), constants.DELAY);
+      setTimeout(() => window.location.replace(NavType.Profile), constants.DELAY);
     } else if (user.auth) {
-      setTimeout(() => navigate(NavType.Expenses, { replace: true }), constants.DELAY);
-    } else setTimeout(() => navigate(NavType.Home, { replace: true }), constants.DELAY);
+      setTimeout(() => window.location.replace(NavType.Expenses), constants.DELAY);
+    } else setTimeout(() => window.location.replace(NavType.Home), constants.DELAY);
   }, []);
   return <IonSpinner className="spinner-center" name="lines-sharp-small"></IonSpinner>;
 }
