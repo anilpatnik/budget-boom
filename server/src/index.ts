e8af7086afbe0152aecd201ef9261fb727a59f57
Anil Patnaik <anil.patnik@gmail.com>
2025-07-15 02:52:05 +1000

    load more with cursor

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,5 +14,5 @@ if (process.env.ENVIRONMENT !== "production") {
     console.log(`${process.env.ENVIRONMENT} server: https://localhost:${PORT}`)
   );
 } else {
-  exports.api = firebase.onRequest(app);
+  exports.apius = firebase.onRequest(app);
 }
