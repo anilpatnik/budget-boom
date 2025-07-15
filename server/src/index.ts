c638e38465a55173b09fa42c058ee6dc98415286
Anil Patnaik <anil.patnik@gmail.com>
2025-07-15 16:12:02 +1000

    refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,5 +20,5 @@ if (process.env.ENVIRONMENT !== "production") {
     console.log(`${process.env.ENVIRONMENT} server: https://localhost:${PORT}`)
   );
   */
-  exports.iap = firebase.onRequest(app);
+  exports.api = firebase.onRequest(app);
 }
