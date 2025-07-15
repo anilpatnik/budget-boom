c02b74ca9a6fb0d017cc6a2d538dbfcb27c5deec
Anil Patnaik <anil.patnik@gmail.com>
2025-07-15 16:39:22 +1000

    refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,4 @@
-// import { https as firebase } from "firebase-functions";
+import { https as firebase } from "firebase-functions";
 import app from "./app";
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`${process.env.ENVIRONMENT} server: https://localhost:${PORT}`));
-
-// exports.api = firebase.onRequest(app);
+exports.api = firebase.onRequest(app);
