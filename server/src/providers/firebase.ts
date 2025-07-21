23052aad475d5f698c8b5022798f2cc0991760f1
Anil Patnaik <anil.patnik@gmail.com>
2025-07-21 12:15:23 +1000

    refactor yaml

--- server/src/providers/firebase.ts
+++ server/src/providers/firebase.ts
@@ -1,13 +1,18 @@
-import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
+import { initializeApp, applicationDefault, cert, Credential } from "firebase-admin/app";
 import { getAuth, UserInfo, CreateRequest, UpdateRequest } from "firebase-admin/auth";
 import { GOOGLE_CREDENTIALS } from "../utils/configs";
 
-const defaultApp = initializeApp({
-  credential: GOOGLE_CREDENTIALS
+let credential: Credential;
+try {
+  credential = GOOGLE_CREDENTIALS
     ? cert(JSON.parse(Buffer.from(GOOGLE_CREDENTIALS, "base64").toString("utf-8")))
-    : applicationDefault()
-});
+    : applicationDefault();
+} catch (err) {
+  console.error("🔥 Failed to parse GOOGLE_CREDENTIALS:", err);
+  credential = applicationDefault();
+}
 
+const defaultApp = initializeApp({ credential });
 const firebaseAuth = getAuth(defaultApp);
 
 export { firebaseAuth, UserInfo, CreateRequest, UpdateRequest };
