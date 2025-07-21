e466a83a9b3d73701e57925b3dd27fe3f4495d4c
Anil Patnaik <anil.patnik@gmail.com>
2025-07-21 10:50:14 +1000

    refactor yaml

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "backend",
-  format: ["esm"],
+  format: ["esm", "cjs"],
   target: "node20",
   define: {
     // remove all code where ENVIRONMENT !== "production"
     "process.env.ENVIRONMENT": '"production"'
   },
   dts: false, // not needed for backend apps
+  treeshake: true, // remove unused code
+  minify: true, // minify the output
   sourcemap: true, // good for debugging
   clean: true, // clean before each build
   splitting: false, // single file output
