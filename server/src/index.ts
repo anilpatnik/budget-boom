4646fee3e19cf177531532689e5ceedb812091bd
Anil Patnaik <anil.patnik@gmail.com>
2026-08-04 17:22:27 +1000

    Refactor (#65)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,13 @@
-import https from "https";
-import fs from "fs";
 import app from "./app";
 
 if (process.env.ENVIRONMENT !== "production") {
-  const options = {
-    key: fs.readFileSync("../ssl.key"),
-    cert: fs.readFileSync("../ssl.pem")
-  };
-  const httpsServer = https.createServer(options, app);
   const PORT: number = 44455;
-  httpsServer.listen(PORT, () =>
-    console.log(`${process.env.ENVIRONMENT} server: https://localhost:${PORT}`)
+  app.listen(PORT, () =>
+    console.log(`${process.env.ENVIRONMENT} server: http://localhost:${PORT}`)
   );
 } else {
   const PORT: number = 8080;
   app.listen(PORT, () =>
-    console.log(`${process.env.ENVIRONMENT} server: https://localhost:${PORT}`)
+    console.log(`${process.env.ENVIRONMENT} server listening on port ${PORT}`)
   );
 }
